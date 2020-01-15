35240ac7d0fdbf5a9ea3a5753bb06429991a70ab
@azukisiromochi <azukisiromochi@gmail.com>
2020-01-15 14:16:23 +0900

    Add: グラデーションビルダー画面にグラデーション作成用の乱数メソッドを追加 🌈

--- lib/components/gradient_builder/gradient_builder.dart
+++ lib/components/gradient_builder/gradient_builder.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,12 +12,21 @@ class GradientBuilder extends StatefulWidget {
 }
 
 class _GradientBuilderState extends State<GradientBuilder> {
+  final _random = Random();
+
   @override
   Widget build(BuildContext context) {
     final _themeColor =
         HSLColor.fromColor(widget.backgroundColor).withLightness(0.2).toColor();
+
+    print(_randomHue());
+    print(_randomHue(HSLColor.fromColor(widget.backgroundColor).hue));
+    print(_randomSaturation());
+    print(_randomLightness());
+
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         iconTheme: IconThemeData(color: _themeColor),
         title: Text(
           'Gradient to you',
@@ -32,5 +42,34 @@ class _GradientBuilderState extends State<GradientBuilder> {
       ),
       body: Placeholder(),
     );
+  }
+
+  double _randomHue([double hue]) {
+    if (hue == null) {
+      return _random.nextInt(360).toDouble();
+    }
+
+    final i = _random.nextInt(30);
+    if (hue == 0 && i < 15) {
+      return (360.0 - 1.0) - i;
+    } else {
+      return hue + (i - 15);
+    }
+  }
+
+  double _randomSaturation() {
+    var d = _random.nextDouble();
+    while (d < 0.2) {
+      d = _random.nextDouble();
+    }
+    return d;
+  }
+
+  double _randomLightness() {
+    var d = _random.nextDouble();
+    while (d < 0.1 || 0.9 < d) {
+      d = _random.nextDouble();
+    }
+    return d;
   }
 }
