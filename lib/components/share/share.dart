ed9af64b74f6562b2fab9d1dbc04aadc0cdebb0c
@azukisiromochi <azukisiromochi@gmail.com>
2020-03-10 22:21:58 +0900

    Add: 保存処理を実装 📼

--- lib/components/share/share.dart
+++ lib/components/share/share.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
+import 'package:ext_storage/ext_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
 import 'package:gradient_to_you/l10n/l10n.dart';
 import 'package:gradient_to_you/utils/app_theme_utils.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../../app_store.dart';
 
@@ -9,6 +13,27 @@ class Share extends StatelessWidget {
   const Share({Key key, @required this.store}) : super(key: key);
 
   final AppStore store;
+
+  String timestamp() => DateTime.now().millisecondsSinceEpoch.toString();
+
+  Future<void> _save() async {
+    final buffer = store.pngImage.buffer;
+
+    final permissions =
+        await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+
+    print(permissions);
+
+    final picturesPath = await ExtStorage.getExternalStoragePublicDirectory(
+        ExtStorage.DIRECTORY_PICTURES);
+    final dirPath = '$picturesPath/gradient2u';
+    await Directory(dirPath).create(recursive: true);
+    final filePath = '$dirPath/${timestamp()}.png';
+
+    print(filePath);
+
+    await File(filePath).writeAsBytes(buffer.asUint8List());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +67,23 @@ class Share extends StatelessWidget {
             title: new Text("Share"),
           ),
         ],
+        onTap: (int index) {
+          switch (index) {
+            case 0:
+              {
+                _save();
+              }
+              break;
+
+            case 1:
+              {}
+              break;
+
+            default:
+              {}
+              break;
+          }
+        },
       ),
     );
   }
